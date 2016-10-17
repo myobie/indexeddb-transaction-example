b875cad41530422d6fa8672e0302065ca5ec8626
myobie <me@nathanherald.com>
2016-10-17 12:41:28 +0200

    Added a transaction queue

--- index.js
+++ index.js
@@ -9,6 +9,117 @@ openDb()
     throw e
   })
 
+class TransactionQueue {
+  constructor (db) {
+    this.db = db
+    this.items = []
+  }
+
+  request (tableName, requestFactory, mode = 'r') {
+    const item = new TransactionQueueItem(tableName, requestFactory, mode)
+    this.items.push(item)
+    return item
+  }
+
+  read (tableName, requestFactory) {
+    return this.request(tableName, requestFactory, 'r')
+  }
+
+  write (tableName, requestFactory) {
+    return this.request(tableName, requestFactory, 'rw')
+  }
+
+  run () {
+    let tableNames
+    let mode = 'r'
+
+    try {
+      const allTableNames = this.items.map(item => item.tableName)
+      const tableNamesSet = new Set(allTableNames)
+      tableNames = [...tableNamesSet]
+
+      const modes = this.items.map(item => item.mode)
+      if (modes.indexOf('rw') !== -1) { mode = 'rw' }
+    } catch (e) {
+      return Promise.reject(e)
+    }
+
+    return new Promise((resolve, reject) => {
+      const transaction = this.db.transaction(tableNames, mode)
+      transaction.onerror = e => reject(e)
+      transaction.onabort = e => reject(e)
+
+      function runDown (items, lastResult, cb) {
+        let currentItem = items.shift()
+
+        if (currentItem) {
+          currentItem._run(transaction, (err, result) => {
+            if (err) { return cb(err) }
+            return runDown(items, result, cb)
+          })
+        } else {
+          return cb(null, lastResult)
+        }
+      }
+
+      runDown(this.items.slice(), null, (err, result) => {
+        if (err) {
+          reject(err)
+          transaction.abort()
+          return
+        }
+        transaction.then(() => resolve(result))
+      })
+    })
+  }
+}
+
+TransactionQueue.run = (db, array) => {
+  const queue = new TransactionQueue(db)
+  for (let info of array) {
+    const item = queue.request(info.table, info.request, info.mode)
+    if (info.success) { item.success(info.success) }
+  }
+  return queue.run()
+}
+
+class TransactionQueueItem {
+  constructor (tableName, requestFactory, mode = 'r') {
+    this.tableName = tableName
+    this.requestFactory = requestFactory
+    this.mode = mode
+    this.successFns = []
+    this.request = null
+    this.result = null
+  }
+
+  success (cb) {
+    this.successFns.push(cb)
+    return this
+  }
+
+  _run (tx, cb) {
+    this.request = this.requestFactory(tx.table(this.tableName))
+    this.request.onsuccess = e => {
+      const result = e.target.result
+      let finalResult
+      let lastResult = result
+      for (let fn of this.successFns) {
+        try {
+          let newResult = fn(lastResult)
+          lastResult = newResult
+        } catch (e) {
+          cb(e)
+          return
+        }
+      }
+
+      cb(null, finalResult)
+    }
+    this.onerror = e => { cb(e) }
+  }
+}
+
 class Table {
   constructor (name, db) {
     this.name = name
@@ -38,6 +149,71 @@ class Table {
         reject(e)
       }
     })
+  }
+
+  batch (genFn) {
+    let queue = new TransactionQueue(this.db)
+    let done = false
+
+    while (!done) {
+      queue.
+    }
+  }
+
+  updateQ (id, currentRevision, attributes) {
+    let item
+
+    try {
+      item = Object.assign({}, attributes, { revision: currentRevision + 1, updatedAt: (new Date()) })
+    } catch (e) {
+      return Promise.reject(e)
+    }
+
+    let mergedData
+
+    // const queue = new TransactionQueue(this.db)
+    // queue.read(this.name, table => table.get(id)).success(compareAndMergeData)
+    // queue.write(this.name, table => table.put(mergedData))
+
+    // let queue = this.db.batch(function* (q) {
+    //   let mergedData
+
+    //   let data = yield q.table(this.name).get(id)
+
+    //   if (data.revision === currentRevision) {
+    //     mergedData = Object.assign({}, data, item)
+    //   } else { throw new Error('revision mismatch') }
+
+    //   yield q.table(this.name).put(mergedData)
+
+    //   return mergedData
+    // })
+
+    // return queue.run()
+
+    const promise = TransactionQueue.run(this.db, [
+      {
+        table: this.name,
+        request: table => table.get(id),
+        mode: 'r',
+        success: compareAndMergeData
+      },
+      {
+        table: this.name,
+        request: table => table.put(mergedData),
+        mode: 'rw'
+      }
+    ])
+
+    return promise.then(() => mergedData)
+
+    function compareAndMergeData (data) {
+      if (data.revision === currentRevision) {
+        mergedData = Object.assign({}, data, item)
+      } else {
+        throw new Error(`revision mismatch: currentRevision is '${currentRevision}' while revision on disk is '${data.revision}'`)
+      }
+    }
   }
 
   update (id, currentRevision, attributes) {
@@ -106,7 +282,7 @@ class Database {
     if (mode === 'rw') {
       _mode = 'readwrite'
     } else {
-      _mode = 'read'
+      _mode = 'readonly'
     }
 
     const _tx = this.db.transaction(tableNames, _mode)
